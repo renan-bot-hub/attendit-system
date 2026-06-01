e6550e0c78c2986d242dd79f4ce0ce10ab8e6ada
nashtongco1 <nashtongco25@gmail.com>
2026-06-01 20:47:27 +0800

    push ko

---
+++ Attend-IT-Mobile/Attend-IT-Mobile-main/attendit_mobile/README.md
@@ -0,0 +1,3 @@
+# attendit_mobile
+
+A new Flutter project.
---
+++ Attend-IT-Mobile/Attend-IT-Mobile-main/attendit_mobile/android/app/src/main/kotlin/com/example/attendit_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.attendit_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
